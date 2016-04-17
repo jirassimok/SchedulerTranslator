fc04be5e13d855e8d1d491f25582cab841de35fb
Jacob I. Komissar <git.i@komissar.net>
2016-04-17 17:04:06 -0400

    Removed unnecessary classes, added key methods add_terms and add_depts to Schedb class.

--- code/schedb/schedb.py
+++ code/schedb/schedb.py
@@ -10,50 +10,29 @@ Temporarily put on hold to complete the lesser version of the functions in time
 for course registration.
 
 """
-# from ..fetch import Fetch
+from .department import Dept
 
 
 class Schedb(object):
     def __init__(self):
-        self.terms = []  # List because nothing to index by.
+        self.terms = []  # List because nothing to index by. - could be a set
         self.depts = {}
 
-"""
-class ReadDict(dict):
-    def __init__(self, *args, **kwargs):
-        self.update(*args, **kwargs)
-    def __setitem__(self, key):
-        raise TypeError("'DeptList' object does not support item assignment")
+    def add_terms(self, json_terms):
+        """ Add a list of terms to this schedb. Prevents addition of duplicates.
 
-class WriteDict(dict):
-    def __init__(self, *args, **kwargs):
-        self.update(*args, **kwargs)
-    def __setitem__(self, key):
-        raise TypeError("'SubjList' object does not support indexing")
+        @param json_terms: A term list json.
+        """
+        for term in json_terms:
+            termstring = term["id"]  # could also use term["title"]
+            if termstring not in self.terms:
+                self.terms.append(termstring)
 
-class DualDict(dict):
-    def __new__(cls, *args, **kwargs):
-        dictionary = dict.__new__(cls, *args, **kwargs)
-        print(dictionary)
-        return ReadDict(dictionary), WriteDict(dictionary)
-#"""
+    def add_depts(self, json_dept_list):
+        """ Add a list of departments to this schedb.
 
-
-class Term(object):
-    """ Represents term jsons and stores subject jsons.
-    """
-    def __init__(self, termstring):
-        self.termstring = termstring
-        self.subjects = {}  # index by abbrev
-        self.depts = self.subjects  # for getting, subjects is for setting
-
-
-class Subject(object):
-    """ Represents contents of sections of subject jsons.
-    Stores courses jsons.
-    """
-    def __init__(self, id, long):
-        self.id = id
-        self.long = long
-        self.courses = {}  # index by course number
-
+        @param json_dept_list: A department list json.
+        """
+        for dept in json_dept_list:
+            abbrev = dept["id"]  # Could use short, but id is more likely unique
+            self.depts.setdefault(abbrev, Dept(abbrev, dept["long"]))
