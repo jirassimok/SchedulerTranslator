e6a6791d665ee46241d80da9fb2b990cdc768e10
Jacob I. Komissar <git.i@komissar.net>
2016-04-17 23:51:49 -0400

    Course constructor now requires dept to be explicitly declared. Removed Course.__contains__. Added measures to ensure that no section is missing data if no json sections register as "main sections." Various cosmetic changes.

--- code/schedb/course.py
+++ code/schedb/course.py
@@ -18,8 +18,8 @@ from .section import Section
 
 
 class Course(object):
-    def __init__(self, number, name, desc="NO DESCRIPTION AVAILABLE",
-                 minCredits=1, maxCredits=1, gradeType="normal", *, dept=None):
+    def __init__(self, number, name, desc="[NO DESCRIPTION GIVEN]",
+                 minCredits=1, maxCredits=1, gradeType="normal", *, dept):
         self.dept = dept  # Not used for xml - __eq__ only; may be None
         self.number = str(number)  # should always be string anyway
         self.name = str(name)  # same, as above, but you never know...
@@ -33,11 +33,9 @@ class Course(object):
 
     def __eq__(self, other):
         """ Two courses are equal if they have the same number and department.
-
-        @param other: The object to compare to.
         """
         return type(self) is type(other) and self.number == other.number \
-                and self.dept == other.dept
+            and self.dept == other.dept
 
     def __str__(self):
         string = ['<course number="', self.number, '" name="', self.name,
@@ -50,11 +48,12 @@ class Course(object):
         stringlist = map(str, string)
         return ''.join(stringlist)
 
+    ''' Probably unnecessary, as I can just do "in self.sections".
+    # Has potential uses if I were to extend this project a bunch, though.
     def __contains__(self, item):
-        """ A course contains its sections.
-        @param item: The object that may be in this one.
-        """
-        return isinstance(item, Section) and item in self.sections
+        """ A course contains its sections. """
+        return item in self.sections
+    '''
 
     def add_regblocks(self, regblocksjson):
         """ Parse a regblocks json's two subsections to allow construction
@@ -76,7 +75,7 @@ class Course(object):
 
         regblocks = [regblock["sectionIds"] for regblock in regblocks if
                      regblock["enabled"] == True]
-        jsections = { section["id"]: section for section in sections }
+        jsections = {section["id"]: section for section in sections}
 
         for regblock in regblocks:  # A regblock is a list of crns.
             self.add_section(regblock, jsections)
@@ -90,13 +89,16 @@ class Course(object):
         @param jsections: The sections in the regblocks json.
         """
         new_section = Section()
+        first_subsection = True
         for crn in crns:
             try:
                 jsection = jsections[crn]  # section is a regblocks[section]
             except KeyError:
                 raise KeyError("Missing section: " + crn)
 
-            if len(jsection["sectionNumber"])==3:  # if it's a lecture,
+            # if it's a lecture or missing any information
+            if len(jsection["sectionNumber"]) == 3 or first_subsection:
+                first_subsection = False
                 new_section.add_main_info(jsection)  # add info to object
                 main_section = Section(jsection)
                 if main_section not in self.sections:
@@ -105,6 +107,7 @@ class Course(object):
 
         # Concatenate lab and lecture section numbers
         # TODO: Check if the section is correctly-populated first.
+        if not new_section.is_valid():
+            new_section.add_main_info
         if new_section not in self.sections:  # prevent duplicate sections
             self.sections.append(new_section)
-
