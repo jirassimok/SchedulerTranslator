119937edb76dd9ca905a87c83f50169136b024ff
Adam Goldsmith <adam@adamgoldsmith.name>
2016-05-16 23:47:12 -0400

    Remove/fix obsolete comments in Main

--- Main.py
+++ Main.py
@@ -5,12 +5,10 @@ Author: Jacob Komissar
 
 Date: 2016-04-12
 
-Running this file will read all departments and courses in the local database
-(which must be running on port 8000) to a schedb, and print all the departments
-and regblocks in to io/regblockslist.json, then read that and write a schedb
-to io/new_v1.1.schedb.
-
-ONLY RUN THIS FILE IN "get" MODE.
+Running this file will read all departments and courses in the local
+database to a schedb, and print all the departments and regblocks into
+io/regblockslist.json, then read that and write a schedb to
+io/new_v1.1.schedb.
 
 """
 # Imports from libraries.
@@ -49,69 +47,9 @@ IO_PATH = IO_PATH.rstrip("/")
 JSON_FILE = IO_PATH + "/regblockslist.json"
 SCHEDB_FILE = IO_PATH + "/new_v1.1.schedb"
 
-# DONE: Create a set of classes to represent the json structure.
-# Using algorithms similar to the web->file database reader,
-# Make a web->object database reader.
-# DONE: Then pass a schedbparse to the schedb to parse.
-# DONE: Fix labs/conferences AFTER classes get me easy access to all the data.
-
 # TODO: Make a version of Fetch.get that gets from the file system directly.*
 # This would allow unicode to be read much more nicely, I am fairly certain.
 
-'''
-Plans:
-
-The next major version of Translator.py will be called jsonParser.py.
-
-If I ever redo this, I hope to further separate the Fetcher from the Parser.
-
-Anything preceded by "#" in these plans is to be skipped
-
-#1. jF will get term info.
-#2. MANUALLY select a term. (if automatic, in this file or its own file)
-#    2.1 Term info ("code") will be passed along the parsechain.
-
-1. MANUALLY get term root URL with department list.
-2. jsonFetcher will get the department list
-3. Parse that to python with json.loads
-4. Add depts to the schedb.
-4. Grab ["id"]s
-5. Create Dept objects
-6. Loop over depts:
-
-For each department:
-7. jsonFetcher gets course list and passes it to the schedb
-8. The schedb loops over the courselist
-9. For each course, the schedb passes it to the correct Dept
-10. The dept adds the courses as incomplete course objects
-11. Grab course number lists. [x["id"].split("|")[1] for x in courselist]
-12. Loop over those to get regblocks
-
-
-In here, I could get the course info pages, but the regblocks have enough.
-
-12. jsonFetcher gets regblocks
-Regblock parsing:
-1. start with a regblocks json
-2. get ["sections"]
-3. sort from list to dictionary, using ["id"] (CRN) as key
-2. get ["registrationBlocks"]
-3. Loop over them:
-4.   add a section
-5.   add a section and period to the course for each thing in ["sectionIds"]
-6.
-
-
-4. The Term's department dictionary's keys will be looped over. Each time:
-5. jsonFetcher will place the course list for the department in a file.
-
-
-Mismatches between formats:
-
-course: schedb has min-/max-credits and grade-type
-section: lots
-
-'''
 if RUN_MODE != "get" and RUN_MODE != "parse":
     raise Exception('Invalid run mode: "' + RUN_MODE + '"')
 
